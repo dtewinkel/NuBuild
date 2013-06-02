ec2d65e4f846f4ab9578d90af1b6ff1e6e9bc541
Daniel te Winkel <github@twia.nl>
2013-06-02 21:31:45 +0200

    Added start for unit-test project.

---
+++ MSBuild/Tasks.UnitTests/LogInterceptor.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Build.Framework;
+using JetBrains.Annotations;
+
+namespace Tasks.UnitTests
+{
+   public class LogInterceptor : ILogger
+   {
+      private IEventSource _eventSource;
+
+      public List<BuildEventArgs> BuildEvents { get; private set; }
+
+      public bool LogToConsole { get; set; }
+
+      public LogInterceptor()
+      {
+         BuildEvents = new List<BuildEventArgs>();
+         Verbosity = LoggerVerbosity.Minimal;
+         LogToConsole = true;
+      }
+
+      public void ClearEvents()
+      {
+         BuildEvents.Clear();
+      }
+
+
+
+
+      public void Initialize(IEventSource eventSource)
+      {
+         ClearEvents();
+
+         _eventSource = eventSource;
+
+         _eventSource.AnyEventRaised += EventSourceOnAnyEventRaised;
+         _eventSource.CustomEventRaised += EventSourceOnCustomEventRaised;
+         _eventSource.BuildFinished += EventSourceOnBuildFinished;
+         _eventSource.BuildStarted += EventSourceOnBuildStarted;
+         _eventSource.ErrorRaised += EventSourceOnErrorRaised;
+         _eventSource.MessageRaised += EventSourceOnMessageRaised;
+         _eventSource.ProjectFinished += EventSourceOnProjectFinished;
+         _eventSource.ProjectStarted += EventSourceOnProjectStarted;
+         _eventSource.StatusEventRaised += EventSourceOnStatusEventRaised;
+         _eventSource.TargetFinished += EventSourceOnTargetFinished;
+         _eventSource.TargetStarted += EventSourceOnTargetStarted;
+         _eventSource.TaskFinished += EventSourceOnTaskFinished;
+         _eventSource.TaskStarted += EventSourceOnTaskStarted;
+         _eventSource.WarningRaised += EventSourceOnWarningRaised;
+      }
+
+
+      private void EventSourceOnTargetStarted(object sender, TargetStartedEventArgs targetStartedEventArgs)
+      {
+         LogEvent(LoggerVerbosity.Detailed, "Target Start", targetStartedEventArgs);
+      }
+
+
+      private void EventSourceOnTargetFinished(object sender, TargetFinishedEventArgs targetFinishedEventArgs)
+      {
+         LogEvent(LoggerVerbosity.Detailed, "Target Finish", targetFinishedEventArgs);
+      }
+
+
+      private void EventSourceOnProjectStarted(object sender, ProjectStartedEventArgs projectStartedEventArgs)
+      {
+         LogEvent(LoggerVerbosity.Detailed, "Project Start", projectStartedEventArgs);
+      }
+
+
+      private void EventSourceOnProjectFinished(object sender, ProjectFinishedEventArgs projectFinishedEventArgs)
+      {
+         LogEvent(LoggerVerbosity.Detailed, "Project Finish", projectFinishedEventArgs);
+      }
+
+
+      private void EventSourceOnBuildStarted(object sender, BuildStartedEventArgs buildStartedEventArgs)
+      {
+         LogEvent(LoggerVerbosity.Normal, "Build Start", buildStartedEventArgs);
+      }
+
+
+      private void EventSourceOnBuildFinished(object sender, BuildFinishedEventArgs buildFinishedEventArgs)
+      {
+         LogEvent(LoggerVerbosity.Normal, "Build Finish", buildFinishedEventArgs);
+      }
+
+
+      private void EventSourceOnTaskFinished(object sender, TaskFinishedEventArgs taskFinishedEventArgs)
+      {
+         LogEvent(LoggerVerbosity.Normal, "Task Finish", taskFinishedEventArgs);
+      }
+
+
+      private void EventSourceOnTaskStarted(object sender, TaskStartedEventArgs taskStartedEventArgs)
+      {
+         LogEvent(LoggerVerbosity.Normal, "Task Start", taskStartedEventArgs);
+      }
+
+
+      private void EventSourceOnCustomEventRaised(object sender, CustomBuildEventArgs customBuildEventArgs)
+      {
+         LogEvent(LoggerVerbosity.Normal, "Custom", customBuildEventArgs);
+      }
+
+
+      private void EventSourceOnStatusEventRaised(object sender, BuildStatusEventArgs buildStatusEventArgs)
+      {
+         LogEvent(LoggerVerbosity.Diagnostic, "Status", buildStatusEventArgs);
+      }
+
+
+      private void EventSourceOnWarningRaised(object sender, BuildWarningEventArgs buildWarningEventArgs)
+      {
+         LogEvent(LoggerVerbosity.Normal, "Warning", buildWarningEventArgs, "({0}, {1}) ",
+                      buildWarningEventArgs.LineNumber, buildWarningEventArgs.ColumnNumber);
+      }
+
+
+      private void EventSourceOnMessageRaised(object sender, BuildMessageEventArgs buildMessageEventArgs)
+      {
+         LogEvent(LoggerVerbosity.Normal, "Message", buildMessageEventArgs);
+      }
+
+
+      private void EventSourceOnAnyEventRaised(object sender, BuildEventArgs buildEventArgs)
+      {
+         LogEvent(LoggerVerbosity.Detailed, "Event", buildEventArgs);
+      }
+
+
+      private void EventSourceOnErrorRaised(object sender, BuildErrorEventArgs buildErrorEventArgs)
+      {
+         LogEvent(LoggerVerbosity.Quiet, "Error", buildErrorEventArgs, "({0}, {1}) ",
+                      buildErrorEventArgs.LineNumber, buildErrorEventArgs.ColumnNumber);
+      }
+
+
+      [StringFormatMethod("message")]
+      private void LogEvent(LoggerVerbosity verbosity, [NotNull] string eventName, [NotNull] BuildEventArgs buildevent, [NotNull] string message = "", params object[] parameters)
+      {
+         BuildEvents.Add(buildevent);
+
+         if (LogToConsole && verbosity <= Verbosity)
+         {
+            string formatedMessage = string.Format(message, parameters);
+            Console.WriteLine("{0}: [{1}] {2}{3}", eventName.PadRight(14), buildevent.Timestamp, formatedMessage, buildevent.Message);
+         }
+      }
+
+
+      public void Shutdown()
+      {
+         _eventSource.AnyEventRaised -= EventSourceOnAnyEventRaised;
+         _eventSource.CustomEventRaised -= EventSourceOnCustomEventRaised;
+         _eventSource.BuildFinished -= EventSourceOnBuildFinished;
+         _eventSource.BuildStarted -= EventSourceOnBuildStarted;
+         _eventSource.ErrorRaised -= EventSourceOnErrorRaised;
+         _eventSource.MessageRaised -= EventSourceOnMessageRaised;
+         _eventSource.ProjectFinished -= EventSourceOnProjectFinished;
+         _eventSource.ProjectStarted -= EventSourceOnProjectStarted;
+         _eventSource.StatusEventRaised -= EventSourceOnStatusEventRaised;
+         _eventSource.TargetFinished -= EventSourceOnTargetFinished;
+         _eventSource.TargetStarted -= EventSourceOnTargetStarted;
+         _eventSource.TaskFinished -= EventSourceOnTaskFinished;
+         _eventSource.TaskStarted -= EventSourceOnTaskStarted;
+         _eventSource.WarningRaised -= EventSourceOnWarningRaised;
+      }
+
+      public LoggerVerbosity Verbosity { get; set; }
+      public string Parameters { get; set; }
+   }
+}
---
+++ MSBuild/Tasks.UnitTests/NuPrepareTests.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Xml;
+using JetBrains.Annotations;
+using Microsoft.Build.Construction;
+using Microsoft.Build.Execution;
+using Microsoft.Build.Framework;
+using NUnit.Framework;
+
+namespace Tasks.UnitTests
+{
+   [TestFixture]
+   public class NuPrepareTests
+   {
+      private const string _projectXml = "<?xml version='1.0' encoding='utf-8'?>\n" +
+                                         "<Project xmlns='http://schemas.microsoft.com/developer/msbuild/2003' ToolsVersion='4.0' >\n" +
+                                         "  <Target Name='build'>\n" +
+                                         "{0}" +
+                                         "    </NuPrepare>\n" +
+                                         "  </Target>\n" +
+                                         "</Project>\n";
+
+      [Test]
+      public void TestSomething()
+      {
+         const string taskXml = "    <NuPrepare NuSpec='{0}'\n" +
+                                "               ProjectName='Test'\n" +
+                                "               VersionSource='Manual'\n" +
+                                "               OutputPath='{1}'\n" +
+                                "               BuildNumber='12'\n" +
+                                "               ReferenceLibraries=''>\n" +
+                                "      <Output TaskParameter='NuSpec' ItemName='NuPrepared'/>\n" +
+                                "      <Output TaskParameter='Sources' ItemName='NuSources'/>\n" +
+                                "      <Output TaskParameter='Targets' ItemName='NuTargets'/>\n";
+
+         const string nuGetSpecXml = "<?xml version='1.0' encoding='utf-8'?>\n" +
+                                    "<package xmlns='http://schemas.microsoft.com/packaging/2010/07/nuspec.xsd'>\n" +
+                                    "  <metadata>\n" +
+                                    "\n" +
+                                    "  </metadata>\n" +
+                                    "</package>\n";
+
+         string processedSpec;
+         BuildResult result = RunTask(nuGetSpecXml, out processedSpec, taskXml);
+         Assert.AreEqual(result.OverallResult, BuildResultCode.Success);
+      }
+
+      [NotNull]
+      BuildResult RunTask([NotNull] string nuGetSpec, out string processedSpec, [NotNull] string taskXml)
+      {
+         string inputFileName = Path.GetTempPath() + Guid.NewGuid().ToString() + ".nuspec";
+         string outputFileName = Path.GetTempPath() + Guid.NewGuid().ToString() + ".nuspec";
+
+         try
+         {
+            File.WriteAllText(inputFileName, nuGetSpec, Encoding.UTF8);
+
+            string formattedTaskXml = string.Format(taskXml, inputFileName, outputFileName);
+            string xml = string.Format(_projectXml, formattedTaskXml);
+            ProjectRootElement rootElement = ProjectRootElement.Create(new XmlTextReader(new StringReader(xml)));
+            rootElement.AddUsingTask("NuBuild.MSBuild.NuPrepare", "NuBuild.MSBuild.Tasks.dll", null);
+
+            ProjectInstance project = new ProjectInstance(rootElement);
+            BuildRequestData requestData = new BuildRequestData(project, new[]
+               {
+                  "build"
+               });
+            BuildParameters buildParameters = new BuildParameters
+               {
+                  Loggers = new List<ILogger>
+                     {
+                        new LogInterceptor()
+                     }
+               };
+
+            BuildResult result = BuildManager.DefaultBuildManager.Build(buildParameters, requestData);
+
+            processedSpec = null;
+            if (File.Exists(outputFileName))
+            {
+               processedSpec = File.ReadAllText(outputFileName);
+            }
+
+            return result;
+         }
+         finally
+         {
+            if (File.Exists(inputFileName))
+            {
+               File.Delete(inputFileName);
+            }
+            if (File.Exists(outputFileName))
+            {
+               File.Delete(outputFileName);
+            }
+         }
+      }
+   }
+}
