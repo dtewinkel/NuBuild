846e26aa746bbea7b68a227f83b32e2e2fe96571
Daniel te Winkel <daniel.tewinkel@nice.com>
2013-06-24 16:16:34 +0200

    Refactoring of unit tests to get out common code to test different task types.

--- MSBuild/Tasks.UnitTests/NuPrepareTests.cs
+++ MSBuild/Tasks.UnitTests/NuPrepareTests.cs
@@ -1,69 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
-using JetBrains.Annotations;
+﻿using JetBrains.Annotations;
 using Microsoft.Build.Framework;
-using Microsoft.Build.Utilities;
 using NUnit.Framework;
 using NuBuild.MSBuild;
 
 namespace Tasks.UnitTests
 {
    [TestFixture]
-   public class NuPrepareTests
+   public class NuPrepareTests : TaskTestBase
    {
-      [NotNull]
-      private readonly FakeBuildEngine _buildEngine = new FakeBuildEngine();
-
-      [NotNull]
-      private readonly List<string> _filesToClean = new List<string>();
-
-      private string _basePath;
-
-
-      [TestFixtureSetUp]
-      public void TestFixtureSetUp()
-      {
-         _basePath = Path.Combine(Path.GetTempPath(), "NuBuild.UnitTests\\");
-         if (!Directory.Exists(_basePath))
-         {
-            Directory.CreateDirectory(_basePath);
-         }
-         Assert.That(Directory.Exists(_basePath));
-      }
-
-
-      [TestFixtureTearDown]
-      public void TestFixtureTearDown()
-      {
-         if (Directory.Exists(_basePath))
-         {
-            Directory.Delete(_basePath, true);
-         }
-      }
-
-
-      [SetUp]
-      public void TestSetUp()
-      {
-         _buildEngine.ClearLoggedEvents();
-      }
-
-
-      [TearDown]
-      public void TearDown()
-      {
-         foreach (string fileName in _filesToClean)
-         {
-            if (File.Exists(fileName))
-            {
-               File.Delete(fileName);
-            }
-         }
-      }
-
-
       [Test]
       public void TestSomething()
       {
@@ -78,10 +22,10 @@ namespace Tasks.UnitTests
          NuPrepare task = CreateTask(nuGetSpecXml);
          bool success = task.Execute();
          Assert.That(success, Is.True);
-         Assert.That(_buildEngine.LogErrorEvents.Count, Is.EqualTo(0));
-         Assert.That(_buildEngine.LogWarningEvents.Count, Is.EqualTo(0));
-         Assert.That(_buildEngine.LogCustomEvents.Count, Is.EqualTo(0));
-         Assert.That(_buildEngine.LogMessageEvents.Count, Is.EqualTo(0));
+         Assert.That(BuildEngine.LogErrorEvents.Count, Is.EqualTo(0));
+         Assert.That(BuildEngine.LogWarningEvents.Count, Is.EqualTo(0));
+         Assert.That(BuildEngine.LogCustomEvents.Count, Is.EqualTo(0));
+         Assert.That(BuildEngine.LogMessageEvents.Count, Is.EqualTo(0));
       }
 
 
@@ -91,35 +35,34 @@ namespace Tasks.UnitTests
          NuPrepare task = CreateTask("");
          bool success = task.Execute();
          Assert.That(success, Is.False);
-         Assert.That(_buildEngine.LogErrorEvents.Count, Is.EqualTo(1));
-         Assert.That(_buildEngine.LogErrorEvents[0].Message, Contains.Substring("XmlException"));
-         Assert.That(_buildEngine.LogWarningEvents.Count, Is.EqualTo(0));
-         Assert.That(_buildEngine.LogCustomEvents.Count, Is.EqualTo(0));
-         Assert.That(_buildEngine.LogMessageEvents.Count, Is.EqualTo(0));
+         Assert.That(BuildEngine.LogErrorEvents.Count, Is.EqualTo(1));
+         Assert.That(BuildEngine.LogErrorEvents[0].Message, Contains.Substring("XmlException"));
+         Assert.That(BuildEngine.LogWarningEvents.Count, Is.EqualTo(0));
+         Assert.That(BuildEngine.LogCustomEvents.Count, Is.EqualTo(0));
+         Assert.That(BuildEngine.LogMessageEvents.Count, Is.EqualTo(0));
       }
 
 
       [NotNull]
       private NuPrepare CreateTask([NotNull] string nuGetSpec)
       {
-         string inputFileName = Path.Combine(_basePath, Guid.NewGuid().ToString() + ".nuspec");
-         _filesToClean.Add(inputFileName);
-         string outputPath = _basePath;
+         ITaskItem taskItem = CreateTaskItem(BasePath, nuGetSpec);
+         return CreateTask(BasePath, new[] { taskItem });
+      }
 
-         File.WriteAllText(inputFileName, nuGetSpec, Encoding.UTF8);
 
-         ITaskItem taskItem = new TaskItem(inputFileName);
+      [NotNull]
+      private NuPrepare CreateTask([NotNull] string basePath, [NotNull] ITaskItem[] nuSpecTaskItems,
+         int buildNumber = 1, string versionSource = "Manual")
+      {
          return new NuPrepare
-            {
-               BuildEngine = _buildEngine,
-               NuSpec = new[]
-                  {
-                     taskItem
-                  },
-               OutputPath = outputPath,
-               BuildNumber = 12,
-               VersionSource = "Manual"
-            };
+         {
+            BuildEngine = BuildEngine,
+            NuSpec = nuSpecTaskItems,
+            OutputPath = basePath,
+            BuildNumber = buildNumber,
+            VersionSource = versionSource
+         };
       }
    }
 }
